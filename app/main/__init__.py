c4db18c36ce6179cdb91b4711c472f11750dc08d
Eddie Chapman <eddie.chapman4@gmail.com>
2022-06-21 13:54:33 -0500

    add database CLI commands

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -14,10 +14,13 @@ from app.main import routes
 
 
 @bp.cli.command("import-articles")
-@click.option("--csv_file", type=click.Path(exists=True, dir_okay=False, path_type=pathlib.Path))
+@click.option("--csv_file", 
+    type=click.Path(exists=True, dir_okay=False, path_type=pathlib.Path),
+    help="The full filepath of a CSV export from a Zotero connection.")
 def import_articles(csv_file):
-    db.create_all()
-
+    """
+    Load new articles into the database
+    """
     with csv_file.open("r", encoding="utf-8-sig") as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -30,3 +33,27 @@ def import_articles(csv_file):
                 db.session.rollback()
             else:
                 print("Session committed successfully")
+
+
+@bp.cli.command("init-db")
+def init_db():
+    """
+    Initialize the database
+    """
+    db.create_all()
+
+
+@bp.cli.command("delete-db")
+def delete_db():
+    """
+    Remove all data from database
+    """
+    db.drop_all()
+
+
+@bp.cli.command("create-article-table")
+def create_article_table():
+    """
+    Create the article table in the database
+    """
+    Article.__table__.create(db.engine)
